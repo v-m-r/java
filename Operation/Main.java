d12c790f16be206c9405d87f24df9e8ee1c95571
v-m-r <manideepreddyvemulapati@gmail.com>
2024-01-05 22:09:50 +0530

    recent Add

---
+++ Operation/Main.java
@@ -0,0 +1,17 @@
+class Main{
+  public static void main(String arg[]){
+    //this program show all the opertions which are present in java
+    int a=1;
+    int b=2;
+    System.out.println("a + b = "+(a+b));
+    System.out.println("a - b = "+(a-b));
+    System.out.println("a x b = "+(a*b));
+    System.out.println("a / b = "+(a/b));
+    System.out.println("a % b = "+(a%b));
+    System.out.println("a & b = "+(a&b));
+    System.out.println("a | b = "+(a|b));
+    System.out.println("a ^ b = "+(a^b));
+    System.out.println("a==b = "+(a==b));
+
+  }
+}
