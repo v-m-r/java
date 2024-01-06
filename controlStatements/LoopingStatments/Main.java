041b39bd8f6fd45735bbd8d0043e38601a6e2e06
v-m-r <manideepreddyvemulapati@gmail.com>
2024-01-06 10:38:09 +0530

    recent Commit

--- controlStatements/LoopingStatments/Main.java
+++ controlStatements/LoopingStatments/Main.java
@@ -1,28 +1,34 @@
 class Main{
   public static void main(String arg[]){
-    //this program describes how to use looping statements in your program
-    //we will be talking about while loop
-    //while is infinite loop if there is no condition so we have close it with a condition unless until we have an idea of what we are doing
-    int i =0;
-    while(i<11){
-      System.out.println("count: "+i);
-      i++;
-      //the keyword break is also a flow control statements which break the flow of program, loop, execution etc.,
+    int i=0;
+    //syntex and writing of while loop
+    System.out.println("At While loop: ");
+    while(i<10){
+      System.out.println("Count: "+i);
+     i++;
     }
-    // at this point we will be seeing how do While loop works the do while loop is a loop which executes first and checks condition next it goes like this
-    i = 0;
+    //syntex and writing of do-while loop
+    System.out.println("At do while loop: ");
     do{
+      System.out.println("count: "+i);
+      i--;
+    }while(i>0);
+    //syntax and way of writing of For loop with initialization;condition;operation
+    System.out.println("At for loop: ");
+    for(i=0;i<10;i++)
     System.out.println("count: "+i);
-      i++;
-    }while(i<10);
-    //we have another looping statement called for loop here is how it is written and executed
-    System.out.println("Reversed numbers");
-    for(i=10;i>0;i++)
-    System.out.println("count: "+i);
-    //we can also write also have for each loop in java programming language which writteng for(obj o:objArray)
-    //we can also use for in an infinite loop without giving any thing in the braces except";;"for(;;){
-    //program comes here
-    //}
-    
+    //syntax and way of writing of For loop infinite form
+    System.out.println("At infinite for loop: ");
+    for(;;){
+      System.out.println("count: "+ i);
+      i--;
+      if (i == 0)
+      break;
+    }
+    //syntax and way of writing of For each loop form
+    System.out.println("At for:each loop: ");
+    for(String j:arg){
+      System.out.println("in argument: "+j);
+    }
   }
 }
