0b8bed0fe84407b70885c7bc7515d2029957f004
v-m-r <manideepreddyvemulapati@gmail.com>
2024-01-09 20:09:58 +0530

    RecentCommit

---
+++ classAndObjects/VarArgs.java
@@ -0,0 +1,15 @@
+public class VarArgs {
+  static void Disp(int ...x){
+  for(int i : x)
+    System.out.print(i+" ");
+  System.out.println();
+  }
+  public static void main(String[] args) {
+    int n[] = {1,2,3,4};
+    int o[] = {5,6,7};
+    int p[] = {7,9,0};
+    Disp(n);
+    Disp(o);
+    Disp(p);
+  }
+}
