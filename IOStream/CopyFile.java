87cd36c8545280b1a401088e98cf8b5252be1ecb
v-m-r <manideepreddyvemulapati@gmail.com>
2024-01-14 22:14:46 +0530

    Modified

--- IOStream/CopyFile.java
+++ IOStream/CopyFile.java
@@ -3,19 +3,13 @@ import java.io.*;
 public class CopyFile{
   public static void main(String[] args) {
     int i;
-    FileInputStream fin = null;
-    FileOutputStream fout = null;
     if(args.length!=2){
       System.out.println("Usage: ShowFile filename");
       return;
     }
-    try {
-      fin = new FileInputStream(args[0]);
-      fout = new FileOutputStream(args[1]);
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    try{
+    try(FileInputStream fin = new FileInputStream(args[0]);
+  FileOutputStream fout = new FileOutputStream(args[1]))
+  {
       do{
         i = fin.read();
         if(i!=-1)fout.write(i);
@@ -23,16 +17,6 @@ public class CopyFile{
     }
     catch(Exception e){
       System.out.println(e);
-    }
-    try {
-      fin.close();
-    } catch (Exception e) {
-      System.out.println("File Not Closed");
-    }
-    try {
-      fout.close();
-    } catch (Exception e) {
-      System.out.println("File Not Closed");
     }
   }
 }
--- IOStream/ShowFile.java
+++ IOStream/ShowFile.java
@@ -2,17 +2,11 @@ import java.io.*;
 public class ShowFile {
   public static void main(String[] args) {
     int i;
-    FileInputStream fin = null;
     if(args.length!=1){
       System.out.println("Usage: ShowFile filename");
       return;
     }
-    try {
-      fin = new FileInputStream(args[0]);
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    try{
+    try(FileInputStream fin = new FileInputStream(args[0])){
       do{
         i = fin.read();
         System.out.print((char)i);
@@ -20,11 +14,6 @@ public class ShowFile {
     }
     catch(Exception e){
       System.out.println(e);
-    }
-    try {
-      fin.close();
-    } catch (Exception e) {
-      System.out.println("File Not Closed");
     }
   }
 }
